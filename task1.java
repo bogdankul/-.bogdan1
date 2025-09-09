b91169d029bdf3dab1fafd2e3b7f32feec682ee2
bogdankul <kulabinb@gmail.com>
2025-09-09 13:15:47 +0300

    Add files via upload

---
+++ task1.java
@@ -0,0 +1,17 @@
+#создание массива данных типа "список" через Java
+
+ArrayList<String> fruits = new ArrayList<String>();
+fruits.add("apple");
+fruits.add("banana");
+fruits.add("watermelon");
+
+#организация стэка через Java
+
+Stack<Integer> s = new Stack<>();
+
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+
+
