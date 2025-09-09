4c89e5c0145fcd44ab2fbeb66a4b3a865dda87c3
bogdankul <kulabinb@gmail.com>
2025-09-09 12:53:52 +0300

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,11 @@
+#создание массива данных типа "список" через python
+
+lst = [1, 2, 3, 4]
+
+#организация стэка через python
+
+stack = []
+
+stack.append("1")
+stack.append("2")
+stack.append("3")
